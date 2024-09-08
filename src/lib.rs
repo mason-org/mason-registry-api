7629022a8f65f061af5a37c254527f8ba9dcda2e
William Boman <william@redwill.se>
2024-09-08 18:16:58 +0200

    chore(api): raise PublicLong cache duration from 1h to 1d

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ impl CacheControl {
             CacheControl::NoStore => "no-store",
             CacheControl::PublicShort => "max-age=0, s-maxage=60, stale-while-revalidate=120",
             CacheControl::PublicMedium => "max-age=0, s-maxage=900",
-            CacheControl::PublicLong => "max-age=0, s-maxage=3600",
+            CacheControl::PublicLong => "max-age=0, s-maxage=86400",
         }
     }
 }
